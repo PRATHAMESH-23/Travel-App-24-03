a417962c38754d6693075c98897847c2faec180d
Prathamesh <prathameshdabhole23@gmail.com>
2025-03-24 22:21:21 +0530

    Initial Project Setup with adding constants

---
+++ lib/config/constants.dart
@@ -0,0 +1,42 @@
+import 'dart:ui';
+
+class AppColors {
+  static const titleText = Color(0xFF000000);
+  static const subText = Color(0xFFAAAAAA);
+  static const orange = Color(0xFFFF7D0D);
+  static const textFieldBorder = Color(0xFFEEEEEE);
+}
+
+class Fonts {
+  static const regular = 'Marcellus Regular';
+  static const medium = 'Gilroy Medium';
+  static const semiBold = 'Gilroy Bold';
+}
+
+class WidgetSize {
+  static const borderRadius_50 = 50.0;
+  static const borderRadius_32 = 32.0;
+  static const borderRadius_24 = 24.0;
+  static const borderRadius_16 = 16.0;
+  static const borderRadius_8 = 8.0;
+  static const borderRadius_4 = 4.0;
+  static const fontSize_10 = 10.0;
+  static const fontSize_12 = 12.0;
+  static const fontSize_13 = 13.0;
+  static const fontSize_14 = 14.0;
+  static const fontSize_16 = 16.0;
+  static const fontSize_18 = 18.0;
+  static const fontSize_20 = 20.0;
+  static const fontSize_24 = 24.0;
+  static const fontSize_28 = 28.0;
+}
+
+class Images {
+  static const logo = 'assets/images/logo.png';
+  static const onBoard1 = 'assets/images/onboard1.png';
+  static const onBoard2 = 'assets/images/onboard2.png';
+  static const onBoard3 = 'assets/images/onboard3.png';
+  static const element1 = 'assets/images/element1.png';
+  static const element2 = 'assets/images/element2.png';
+  static const element3 = 'assets/images/element3.png';
+}
